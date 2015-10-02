1a6905facba065891b9837c323f5c9760737c947
mcyee <mingcee@skysketches.com>
2015-10-02 13:42:16 -0400

    finished initializing continents

--- src/com/skysketches/RiskAttackSequence/GameState.java
+++ src/com/skysketches/RiskAttackSequence/GameState.java
@@ -48,57 +48,57 @@ public class GameState {
 	/**
 	 * initContinents() initialises the Territories in all the continents
 	 */
-	private Boolean initContinents() {
+	private void initContinents() {
 		// add Africa
 		List<Territory> africa = new ArrayList<Territory>();
-		List<String> territories = new ArrayList<String>();
 		
 		// add Congo to Africa
-		territories.add("East Africa");
-		territories.add("North Africa");
-		territories.add("South Africa");
-		africa.add(new Territory(territories, "Congo"));
+		List<String> congoTerr = new ArrayList<String>();
+		congoTerr.add("East Africa");
+		congoTerr.add("North Africa");
+		congoTerr.add("South Africa");
+		africa.add(new Territory(congoTerr, "Congo"));
 		
 		// add East Africa to Africa
-		territories.clear();
-		territories.add("Congo");
-		territories.add("Egypt");
-		territories.add("Madagascar");
-		territories.add("North Africa");
-		territories.add("South Africa");
-		territories.add("Middle East");
-		africa.add(new Territory(territories, "East Africa"));
+		List<String> eafricaTerr = new ArrayList<String>();
+		eafricaTerr.add("Congo");
+		eafricaTerr.add("Egypt");
+		eafricaTerr.add("Madagascar");
+		eafricaTerr.add("North Africa");
+		eafricaTerr.add("South Africa");
+		eafricaTerr.add("Middle East");
+		africa.add(new Territory(eafricaTerr, "East Africa"));
 		
 		// add Egypt to Africa
-		territories.clear();
-		territories.add("East Africa");
-		territories.add("North Africa");
-		territories.add("Southern Europe");
-		territories.add("Middle East");
-		africa.add(new Territory(territories, "Egypt"));
+		List<String> egyptTerr = new ArrayList<String>();
+		egyptTerr.add("East Africa");
+		egyptTerr.add("North Africa");
+		egyptTerr.add("Southern Europe");
+		egyptTerr.add("Middle East");
+		africa.add(new Territory(egyptTerr, "Egypt"));
 		
 		// add Madagascar to Africa
-		territories.clear();
-		territories.add("East Africa");
-		territories.add("South Africa");
-		africa.add(new Territory(territories, "Madagascar"));
+		List<String> madagascarTerr = new ArrayList<String>();
+		madagascarTerr.add("East Africa");
+		madagascarTerr.add("South Africa");
+		africa.add(new Territory(madagascarTerr, "Madagascar"));
 		
 		// add North Africa to Africa
-		territories.clear();
-		territories.add("Congo");
-		territories.add("East Africa");
-		territories.add("Egypt");
-		territories.add("Brazil");
-		territories.add("Southern Europe");
-		territories.add("Western Europe");
-		africa.add(new Territory(territories, "North Africa"));
+		List<String> nafricaTerr = new ArrayList<String>();
+		nafricaTerr.add("Congo");
+		nafricaTerr.add("East Africa");
+		nafricaTerr.add("Egypt");
+		nafricaTerr.add("Brazil");
+		nafricaTerr.add("Southern Europe");
+		nafricaTerr.add("Western Europe");
+		africa.add(new Territory(nafricaTerr, "North Africa"));
 		
 		// add South Africa to Africa
-		territories.clear();
-		territories.add("Congo");
-		territories.add("East Africa");
-		territories.add("Madagascar");
-		africa.add(new Territory(territories, "South Africa"));
+		List<String> safricaTerr = new ArrayList<String>();
+		safricaTerr.add("Congo");
+		safricaTerr.add("East Africa");
+		safricaTerr.add("Madagascar");
+		africa.add(new Territory(safricaTerr, "South Africa"));
 		
 		this.continents.add(africa);
 		
@@ -106,31 +106,31 @@ public class GameState {
 		List<Territory> australia = new ArrayList<Territory>();
 		
 		// add Eastern Australia to Australia
-		territories.clear();
-		territories.add("New Guinea");
-		territories.add("Western Australia");
-		australia.add(new Territory(territories, "Eastern Australia"));
+		List<String> eaustraliaTerr = new ArrayList<String>();
+		eaustraliaTerr.add("New Guinea");
+		eaustraliaTerr.add("Western Australia");
+		australia.add(new Territory(eaustraliaTerr, "Eastern Australia"));
 		
 		// add Indonesia to Australia
-		territories.clear();
-		territories.add("New Guinea");
-		territories.add("Western Australia");
-		territories.add("Siam");
-		australia.add(new Territory(territories, "Indonesia"));
+		List<String> indonesiaTerr = new ArrayList<String>();
+		indonesiaTerr.add("New Guinea");
+		indonesiaTerr.add("Western Australia");
+		indonesiaTerr.add("Siam");
+		australia.add(new Territory(indonesiaTerr, "Indonesia"));
 		
 		// add New Guinea to Australia
-		territories.clear();
-		territories.add("Eastern Australia");
-		territories.add("Indonesia");
-		territories.add("Western Australia");
-		australia.add(new Territory(territories, "New Guinea"));
+		List<String> newguineaTerr = new ArrayList<String>();
+		newguineaTerr.add("Eastern Australia");
+		newguineaTerr.add("Indonesia");
+		newguineaTerr.add("Western Australia");
+		australia.add(new Territory(newguineaTerr, "New Guinea"));
 		
 		// add Western Australia to Australia
-		territories.clear();
-		territories.add("Eastern Australia");
-		territories.add("Indonesia");
-		territories.add("New Guinea");
-		australia.add(new Territory(territories, "Western Australia"));
+		List<String> waustraliaTerr = new ArrayList<String>();
+		waustraliaTerr.add("Eastern Australia");
+		waustraliaTerr.add("Indonesia");
+		waustraliaTerr.add("New Guinea");
+		australia.add(new Territory(waustraliaTerr, "Western Australia"));
 		
 		this.continents.add(australia);
 		
@@ -138,104 +138,104 @@ public class GameState {
 		List<Territory> asia = new ArrayList<Territory>();
 		
 		// add Afghanistan to Asia
-		territories.clear();
-		territories.add("China");
-		territories.add("India");
-		territories.add("Ukraine");
-		territories.add("Middle East");
-		territories.add("Ural");
-		asia.add(new Territory(territories, "Afghanistan"));
+		List<String> afghanistanTerr = new ArrayList<String>();
+		afghanistanTerr.add("China");
+		afghanistanTerr.add("India");
+		afghanistanTerr.add("Ukraine");
+		afghanistanTerr.add("Middle East");
+		afghanistanTerr.add("Ural");
+		asia.add(new Territory(afghanistanTerr, "Afghanistan"));
 		
 		// add China to Asia
-		territories.clear();
-		territories.add("Afghanistan");
-		territories.add("India");
-		territories.add("Mongolia");
-		territories.add("Siam");
-		territories.add("Siberia");
-		territories.add("Ural");
-		asia.add(new Territory(territories, "China"));
+		List<String> chinaTerr = new ArrayList<String>();
+		chinaTerr.add("Afghanistan");
+		chinaTerr.add("India");
+		chinaTerr.add("Mongolia");
+		chinaTerr.add("Siam");
+		chinaTerr.add("Siberia");
+		chinaTerr.add("Ural");
+		asia.add(new Territory(chinaTerr, "China"));
 		
 		// add India to Asia
-		territories.clear();
-		territories.add("Afghanistan");
-		territories.add("China");
-		territories.add("Middle East");
-		territories.add("Siam");
-		asia.add(new Territory(territories, "India"));
+		List<String> indiaTerr = new ArrayList<String>();
+		indiaTerr.add("Afghanistan");
+		indiaTerr.add("China");
+		indiaTerr.add("Middle East");
+		indiaTerr.add("Siam");
+		asia.add(new Territory(indiaTerr, "India"));
 		
 		// add Irkutsk to Asia
-		territories.clear();
-		territories.add("Kamchatka");
-		territories.add("Mongolia");
-		territories.add("Siberia");
-		territories.add("Yakutsk");
-		asia.add(new Territory(territories, "Irkutsk"));
+		List<String> irkutskTerr = new ArrayList<String>();
+		irkutskTerr.add("Kamchatka");
+		irkutskTerr.add("Mongolia");
+		irkutskTerr.add("Siberia");
+		irkutskTerr.add("Yakutsk");
+		asia.add(new Territory(irkutskTerr, "Irkutsk"));
 		
 		// add Japan to Asia
-		territories.clear();
-		territories.add("Kamchatka");
-		territories.add("Mongolia");
-		asia.add(new Territory(territories, "Japan"));
+		List<String> japanTerr = new ArrayList<String>();
+		japanTerr.add("Kamchatka");
+		japanTerr.add("Mongolia");
+		asia.add(new Territory(japanTerr, "Japan"));
 		
 		// add Kamchatka to Asia
-		territories.clear();
-		territories.add("Alaska");
-		territories.add("Irkutsk");
-		territories.add("Japan");
-		territories.add("Mongolia");
-		territories.add("Yakutsk");
-		asia.add(new Territory(territories, "Kamchatka"));
+		List<String> kamchatkaTerr = new ArrayList<String>();
+		kamchatkaTerr.add("Alaska");
+		kamchatkaTerr.add("Irkutsk");
+		kamchatkaTerr.add("Japan");
+		kamchatkaTerr.add("Mongolia");
+		kamchatkaTerr.add("Yakutsk");
+		asia.add(new Territory(kamchatkaTerr, "Kamchatka"));
 		
 		// add Middle East to Asia
-		territories.clear();
-		territories.add("Afghanistan");
-		territories.add("East Africa");
-		territories.add("India");
-		territories.add("Egypt");
-		territories.add("Southern Europe");
-		territories.add("Ukraine");
-		asia.add(new Territory(territories, "Middle East"));
+		List<String> middleeastTerr = new ArrayList<String>();
+		middleeastTerr.add("Afghanistan");
+		middleeastTerr.add("East Africa");
+		middleeastTerr.add("India");
+		middleeastTerr.add("Egypt");
+		middleeastTerr.add("Southern Europe");
+		middleeastTerr.add("Ukraine");
+		asia.add(new Territory(middleeastTerr, "Middle East"));
 		
 		// add Mongolia to Asia
-		territories.clear();
-		territories.add("China");
-		territories.add("Irkutsk");
-		territories.add("Japan");
-		territories.add("Kamchatka");
-		territories.add("Siberia");
-		asia.add(new Territory(territories, "Mongolia"));
+		List<String> mongoliaTerr = new ArrayList<String>();
+		mongoliaTerr.add("China");
+		mongoliaTerr.add("Irkutsk");
+		mongoliaTerr.add("Japan");
+		mongoliaTerr.add("Kamchatka");
+		mongoliaTerr.add("Siberia");
+		asia.add(new Territory(mongoliaTerr, "Mongolia"));
 		
 		// add Siam to Asia
-		territories.clear();
-		territories.add("Indonesia");
-		territories.add("China");
-		territories.add("India");
-		asia.add(new Territory(territories, "Siam"));
+		List<String> siamTerr = new ArrayList<String>();
+		siamTerr.add("Indonesia");
+		siamTerr.add("China");
+		siamTerr.add("India");
+		asia.add(new Territory(siamTerr, "Siam"));
 		
 		// add Siberia to Asia
-		territories.clear();
-		territories.add("China");
-		territories.add("Irkutsk");
-		territories.add("Mongolia");
-		territories.add("Ural");
-		territories.add("Yakutsk");
-		asia.add(new Territory(territories, "Siberia"));
+		List<String> siberiaTerr = new ArrayList<String>();
+		siberiaTerr.add("China");
+		siberiaTerr.add("Irkutsk");
+		siberiaTerr.add("Mongolia");
+		siberiaTerr.add("Ural");
+		siberiaTerr.add("Yakutsk");
+		asia.add(new Territory(siberiaTerr, "Siberia"));
 		
 		// add Ural to Asia
-		territories.clear();
-		territories.add("Afghanistan");
-		territories.add("China");
-		territories.add("Ukraine");
-		territories.add("Siberia");
-		asia.add(new Territory(territories, "Ural"));
+		List<String> uralTerr = new ArrayList<String>();
+		uralTerr.add("Afghanistan");
+		uralTerr.add("China");
+		uralTerr.add("Ukraine");
+		uralTerr.add("Siberia");
+		asia.add(new Territory(uralTerr, "Ural"));
 		
 		// add Yakutsk to Asia
-		territories.clear();
-		territories.add("Irkutsk");
-		territories.add("Kamchatka");
-		territories.add("Siberia");
-		asia.add(new Territory(territories, "Yakutsk"));
+		List<String> yakutskTerr = new ArrayList<String>();
+		yakutskTerr.add("Irkutsk");
+		yakutskTerr.add("Kamchatka");
+		yakutskTerr.add("Siberia");
+		asia.add(new Territory(yakutskTerr, "Yakutsk"));
 		
 		this.continents.add(asia);
 		
@@ -243,64 +243,175 @@ public class GameState {
 		List<Territory> europe = new ArrayList<Territory>();
 		
 		// add Great Britain to Europe
-		territories.clear();
-		territories.add("Iceland");
-		territories.add("Northern Europe");
-		territories.add("Scandinavia");
-		territories.add("Western Europe");
-		europe.add(new Territory(territories, "Great Britain"));
+		List<String> gbTerr = new ArrayList<String>();
+		gbTerr.add("Iceland");
+		gbTerr.add("Northern Europe");
+		gbTerr.add("Scandinavia");
+		gbTerr.add("Western Europe");
+		europe.add(new Territory(gbTerr, "Great Britain"));
 		
 		// add Iceland to Europe
-		territories.clear();
-		territories.add("Great Britain");
-		territories.add("Scandinavia");
-		territories.add("Greenland");
-		europe.add(new Territory(territories, "Iceland"));
+		List<String> icelandTerr = new ArrayList<String>();
+		icelandTerr.add("Great Britain");
+		icelandTerr.add("Scandinavia");
+		icelandTerr.add("Greenland");
+		europe.add(new Territory(icelandTerr, "Iceland"));
 		
 		// add Northern Europe to Europe
-		territories.clear();
-		territories.add("Great Britain");
-		territories.add("Scandinavia");
-		territories.add("Southern Europe");
-		territories.add("Ukraine");
-		territories.add("Western Europe");
-		europe.add(new Territory(territories, "Northern Europe"));
+		List<String> neuropeTerr = new ArrayList<String>();
+		neuropeTerr.add("Great Britain");
+		neuropeTerr.add("Scandinavia");
+		neuropeTerr.add("Southern Europe");
+		neuropeTerr.add("Ukraine");
+		neuropeTerr.add("Western Europe");
+		europe.add(new Territory(neuropeTerr, "Northern Europe"));
 		
 		// add Scandinavia to Europe
-		territories.clear();
-		territories.add("Great Britain");
-		territories.add("Iceland");
-		territories.add("Northern Europe");
-		territories.add("Ukraine");
-		europe.add(new Territory(territories, "Scandinavia"));
+		List<String> scandinaviaTerr = new ArrayList<String>();
+		scandinaviaTerr.add("Great Britain");
+		scandinaviaTerr.add("Iceland");
+		scandinaviaTerr.add("Northern Europe");
+		scandinaviaTerr.add("Ukraine");
+		europe.add(new Territory(scandinaviaTerr, "Scandinavia"));
 		
 		// add Southern Europe to Europe
-		territories.clear();
-		territories.add("Northern Europe");
-		territories.add("Egypt");
-		territories.add("North Africa");
-		territories.add("Ukraine");
-		territories.add("Western Europe");
-		territories.add("Middle East");
-		europe.add(new Territory(territories, "Southern Europe"));
+		List<String> seuropeTerr = new ArrayList<String>();
+		seuropeTerr.add("Northern Europe");
+		seuropeTerr.add("Egypt");
+		seuropeTerr.add("North Africa");
+		seuropeTerr.add("Ukraine");
+		seuropeTerr.add("Western Europe");
+		seuropeTerr.add("Middle East");
+		europe.add(new Territory(seuropeTerr, "Southern Europe"));
 		
 		// add Ukraine to Europe
-		territories.clear();
-		territories.add("Afghanistan");
-		territories.add("Northern Europe");
-		territories.add("Scandinavia");
-		territories.add("Southern Europe");
-		territories.add("Middle East");
-		territories.add("Ural");
-		europe.add(new Territory(territories, "Ukraine"));
+		List<String> ukraineTerr = new ArrayList<String>();
+		ukraineTerr.add("Afghanistan");
+		ukraineTerr.add("Northern Europe");
+		ukraineTerr.add("Scandinavia");
+		ukraineTerr.add("Southern Europe");
+		ukraineTerr.add("Middle East");
+		ukraineTerr.add("Ural");
+		europe.add(new Territory(ukraineTerr, "Ukraine"));
 		
 		// add Western Europe to Europe
-		territories.clear();
-		territories.add("Great Britain");
-		territories.add("Northern Europe");
-		territories.add("Southern Europe");
-		territories.add("North Africa");
-		europe.add(new Territory(territories, "Western Europe"));
+		List<String> weuropeTerr = new ArrayList<String>();
+		weuropeTerr.add("Great Britain");
+		weuropeTerr.add("Northern Europe");
+		weuropeTerr.add("Southern Europe");
+		weuropeTerr.add("North Africa");
+		europe.add(new Territory(weuropeTerr, "Western Europe"));
+		
+		this.continents.add(europe);
+		
+		// add North America
+		List<Territory> namerica = new ArrayList<Territory>();
+		
+		// add Alaska to North America
+		List<String> alaskaTerr = new ArrayList<String>();
+		alaskaTerr.add("Alberta");
+		alaskaTerr.add("Northwest Territory");
+		alaskaTerr.add("Kamchatka");
+		namerica.add(new Territory(alaskaTerr, "Alaska"));
+		
+		// add Alberta to North America
+		List<String> albertaTerr = new ArrayList<String>();
+		albertaTerr.add("Alaska");
+		albertaTerr.add("Northwest Territory");
+		albertaTerr.add("Ontario");
+		albertaTerr.add("Western United States");
+		namerica.add(new Territory(albertaTerr, "Alberta"));
+
+		// add Central America to North America
+		List<String> camericaTerr = new ArrayList<String>();
+		camericaTerr.add("Eastern United States");
+		camericaTerr.add("Venezuela");
+		camericaTerr.add("Western United States");
+		namerica.add(new Territory(camericaTerr, "Central America"));
+
+		// add Eastern United States to North America
+		List<String> eusTerr = new ArrayList<String>();
+		eusTerr.add("Central America");
+		eusTerr.add("Ontario");
+		eusTerr.add("Quebec");
+		eusTerr.add("Western United States");
+		namerica.add(new Territory(eusTerr, "Eastern United States"));
+
+		// add Greenland to North America
+		List<String> greenlandTerr = new ArrayList<String>();
+		greenlandTerr.add("Iceland");
+		greenlandTerr.add("Northwest Territory");
+		greenlandTerr.add("Ontario");
+		greenlandTerr.add("Quebec");
+		namerica.add(new Territory(greenlandTerr, "Greenland"));
+
+		// add Northwest Territory to North America
+		List<String> nwterritoryTerr = new ArrayList<String>();
+		nwterritoryTerr.add("Alaska");
+		nwterritoryTerr.add("Alberta");
+		nwterritoryTerr.add("Greenland");
+		nwterritoryTerr.add("Ontario");
+		namerica.add(new Territory(nwterritoryTerr, "Northwest Territory"));
+
+		// add Ontario to North America
+		List<String> ontarioTerr = new ArrayList<String>();
+		ontarioTerr.add("Alberta");
+		ontarioTerr.add("Eastern United States");
+		ontarioTerr.add("Greenland");
+		ontarioTerr.add("Northwest Territory");
+		ontarioTerr.add("Quebec");
+		ontarioTerr.add("Western United States");
+		namerica.add(new Territory(ontarioTerr, "Ontario"));
+
+		// add Quebec to North America
+		List<String> quebecTerr = new ArrayList<String>();
+		quebecTerr.add("Eastern United States");
+		quebecTerr.add("Greenland");
+		quebecTerr.add("Ontario");
+		namerica.add(new Territory(quebecTerr, "Quebec"));
+
+		// add Western United States to North America
+		List<String> wusTerr = new ArrayList<String>();
+		wusTerr.add("Alberta");
+		wusTerr.add("Central America");
+		wusTerr.add("Eastern United States");
+		wusTerr.add("Ontario");
+		namerica.add(new Territory(wusTerr, "Western United States"));
+		
+		this.continents.add(namerica);
+		
+		// add South America
+		List<Territory> samerica = new ArrayList<Territory>();
+		
+		// add Argentina to South America
+		List<String> argentinaTerr = new ArrayList<String>();
+		argentinaTerr.add("Brazil");
+		argentinaTerr.add("Peru");
+		samerica.add(new Territory(argentinaTerr, "Argentina"));
+
+		// add Brazil to South America
+		List<String> brazilTerr = new ArrayList<String>();
+		brazilTerr.add("Argentina");
+		brazilTerr.add("Peru");
+		brazilTerr.add("Venezuela");
+		brazilTerr.add("North Africa");
+		samerica.add(new Territory(brazilTerr, "Brazil"));
+
+		// add Peru to South America
+		List<String> peruTerr = new ArrayList<String>();
+		peruTerr.add("Argentina");
+		peruTerr.add("Brazil");
+		peruTerr.add("Venezuela");
+		samerica.add(new Territory(peruTerr, "Peru"));
+
+		// add Venezuela to South America
+		List<String> venezuelaTerr = new ArrayList<String>();
+		venezuelaTerr.add("Brazil");
+		venezuelaTerr.add("Peru");
+		venezuelaTerr.add("Central America");
+		samerica.add(new Territory(venezuelaTerr, "Venezuela"));
+		
+		this.continents.add(samerica);
 	}
 	
 	/**
